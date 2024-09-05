90b3baf44e34a3df0f68198bc2870a387bfb0579
Swastik-Dutta2004 <duttaswastik004@gmail.com>
2024-09-05 17:48:01 +0530

    Array concept in solidity

---
+++ contracts/arrays.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+contract mycontract {
+
+    uint[] public myuint = [1,2,3];
+    string[] public mystring["swastik"];
+    string[] public values;
+    uint[] public array2d = [[1,2,3][4,5,6]]
+
+
+    function addvalue (string memory _value) public{
+        value.push(_values);
+    }
+    function valuecount() public view returns(uint){
+        return value.lenght; 
+    }
+}
